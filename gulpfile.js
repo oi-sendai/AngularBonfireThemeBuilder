ebd17704128f3511fe05616bd9bd76557e19ba35
oi-sendai <binarygeometry@gmail.com>
2015-04-19 18:11:42 +0100

    added some dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
+// var requireDir = require('require-dir')
+var plumber = require('gulp-plumber')
 var gulp = require('gulp')
 ,   sass = require('gulp-sass')
 ,   sourcemaps = require('gulp-sourcemaps')
 ,   prefix = require('gulp-autoprefixer')
 ,   concat = require('gulp-concat')
 
+// Set the Bonfire theme name
+var themeTemplate = 'default'
+
+// set up default routing for AngularBonfire
+var config = {
+	'templatePath' : './public/themes/'+themeTemplate+'/assets',
+	'modulesPath'  : './application/modules/**/assets'
+}
+// Require all tasks in gulp/tasks, including subfolders
+// requireDir('gulp/tasks', { recurse: true })
+
 gulp.task('sass', function() {
-	gulp.src('./application/modules/**/assets/sass/*.{scss,sass}')
+	// gulp.src('./application/modules/**/assets/sass/*.{scss,sass}')
+	gulp.src([config.modulesPath+'/sass/**.*', config.templatePath+'/sass/**.*'])
+	.pipe(plumber())
 	.pipe(sass())
 	.pipe(prefix('last 2 version', '> 1%', 'ie 8', 'ie 9'))
-	.pipe(concat('style.css'))
-	.pipe(gulp.dest('./css'));
+	// .pipe(concat('style.css'))
+	.pipe(gulp.dest(config.templatePath + './css'));
 })
 
 // Watch scss folder for changes
